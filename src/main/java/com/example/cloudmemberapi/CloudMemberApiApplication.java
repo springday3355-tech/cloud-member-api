718a16e42c66e1bc899e5d85b539b72180a60e63
송이 <springday3355@gmail.com>
2026-05-18 17:20:09 +0900

    first commit

---
+++ src/main/java/com/example/cloudmemberapi/CloudMemberApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.cloudmemberapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudMemberApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudMemberApiApplication.class, args);
+    }
+
+}
